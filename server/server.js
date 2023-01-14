b029d5df3c61533fc5b4f3cda81d1bb55c92a2e9
Jonathan <jonlee121198@gmail.com>
2023-01-14 22:14:02 +0800

    server 1.1

--- server/server.js
+++ server/server.js
@@ -1,20 +1,47 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
-const {errorHandler} = require('./middleware/errorMiddleware')
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-//Middleware for getting body in a request
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    //Controls which domains have access, * means any domain
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+    //Controls which headers are allowed and can be handled
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
 
-app.use(errorHandler)
+    //Controls which http methods are allowed
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//     app.get("*", (req, res) =>
+//         res.sendFile(
+//             path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//         )
+//     );
+// } else {
+//     app.get("/", (req, res) => res.send("Please set to production"));
+// }
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

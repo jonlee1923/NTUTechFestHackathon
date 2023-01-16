e9c92b84f8180a397a58ad42a081d59ceee6d6f6
Eugene Lum <eugenelumtf@gmail.com>
2023-01-16 15:25:51 +0800

    fix app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,62 +2,52 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect, useContext } from "react";
 
-import Home from "./pages/homePage";
-import Login from "./pages/loginPage";
-import Profile from "./pages/profilePage";
-import AppNavbar from "./pages/navbar";
-import Footer from "./pages/footer";
-import EditImage from "./pages/editImage";
+import Home from "./pages/homePage/homePage";
+import Login from "./pages/loginPage/loginPage";
+import Profile from "./pages/profilePage/profilePage";
+import AppNavbar from "./pages/navbar/navbar";
+import Footer from "./pages/footer/footer";
+import EditImage from "./pages/editImage/editImage";
 
 import { AuthContext } from "./context/authContext";
 import { useAuth } from "./hooks/authHook";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
-
 function App() {
-    const { token, userId, isLoggedIn } = useContext(AuthContext);
-    const [logged,setLogged] = useState(false);
-    console.log("app.js", token, userId, isLoggedIn);
+  const { token, userId, isLoggedIn } = useContext(AuthContext);
+  const [logged, setLogged] = useState(false);
+  console.log("app.js", token, userId, isLoggedIn);
 
-    useEffect(()=>{
-      if(token){
-          setLogged(true);
-      }
-      else{
-          setLogged(false);
-      }
-    },[token,userId]);
+  useEffect(() => {
+    if (token) {
+      setLogged(true);
+    } else {
+      setLogged(false);
+    }
+  }, [token, userId]);
 
-    return (
-        <>
-            <AppNavbar />
-            <BrowserRouter>
-                {!logged ? (
-                    <Login/>
-                ) : (
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route
-                            path="/profile"
-                            element={
-                                isLoggedIn ? <Profile /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route
-                            path="/editimage"
-                            element={
-                                isLoggedIn ? <EditImage /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/editimage" element={<EditImage />} />
-                    </Routes>
-                )}
-            </BrowserRouter>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <AppNavbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/profile"
+            element={logged ? <Profile /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/editimage"
+            element={logged ? <EditImage /> : <Navigate to="/login" />}
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/editimage" element={<EditImage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;

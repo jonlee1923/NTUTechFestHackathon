8a193f131fa111c5b3ab30d0c0925794ee03d8b9
Jonathan <jonlee121198@gmail.com>
2023-01-17 09:53:49 +0800

    added

--- client/src/components/Skills/Skills.jsx
+++ client/src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container, Card, Row, Col } from "react-bootstrap";
 import { Github, Journal, JournalAlbum } from "react-bootstrap-icons";
@@ -9,30 +9,22 @@ import classes from "./Skills.module.css";
 
 const Skills = (props) => {
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [skills, setSkills] = useState("");
 
     const handleSubmit = async (event) => {
         try {
-
             event.preventDefault();
-            // Send a request to the server with the email and password
-            let url = "http://localhost:8000/predict";
-            let method = "POST";
-            let body = JSON.stringify({
-                input: "testing java hadoop ruby scala",
-            });
-            let headers = {
-                "Content-Type": "application/json",
-            };
-
-            headers.append(
-                "Access-Control-Allow-Origin",
-                "http://localhost:3000"
-            );
-            console.log("hi")
-
-            console.log("clicked")
-            const response = await sendRequest(url, method, body, headers);
-            console.log(response);
+            await fetch("http://localhost:8000/predict", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    input: `${skills}`,
+                }),
+            })
+                .then((res) => res.json())
+                .then((response) => console.log(response));
         } catch (err) {}
     };
 
--- server/MlModel/apiCaller.py
+++ server/MlModel/apiCaller.py
@@ -3,6 +3,6 @@ import requests
 
 data = {'input': 'testing java hadoop ruby scala'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-response = requests.post('http://localhost:8000/predict', data=json.dumps(data), headers=headers)
+response = requests.post('http://127.0.0.1:8000/predict', data=json.dumps(data), headers=headers)
 
 print(response.json())
